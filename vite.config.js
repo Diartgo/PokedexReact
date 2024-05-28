a1df105360365b3fcef33dddf3fda28b7d9b7c9f
Diana <diaartgo@gmail.com>
2024-05-28 14:42:09 -0500

    desplegando en github pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-  server: {
-    port: 3000
-  },
   plugins: [react()],
-  base: '/PokedexReact/'
+  base: 'https://diartgo.github.io/PokedexReact/'
 })
